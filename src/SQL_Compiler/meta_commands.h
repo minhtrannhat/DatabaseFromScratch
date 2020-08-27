fdd6e2be77fc4c2a7b808397db013b44bdf17928
minhradz <minhradz@gmail.com>
2020-08-27 16:10:19 -0400

    Feat: Preparation for the SQL compiler and the SQL VM

---
+++ src/SQL_Compiler/meta_commands.h
@@ -0,0 +1,9 @@
+typedef enum {
+  META_COMMAND_SUCCESS,
+  META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
+typedef enum {
+  PREPARE_SUCCESS,
+  PREPARE_UNRECOGNIZED_STATEMENT
+} PrepareResult;
